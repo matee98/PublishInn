0ba8b711bb1e185fb79dd21c93919c43364f8e6c
224464 Zabroń Mateusz <matzab37@gmail.com>
2021-10-27 16:56:49 +0200

    Repository initialization.

---
+++ src/main/java/com/github/PublishInn/PublishInnApplication.java
@@ -0,0 +1,13 @@
+package com.github.PublishInn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishInnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublishInnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/PublishInn/PublishInnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.PublishInn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishInnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
