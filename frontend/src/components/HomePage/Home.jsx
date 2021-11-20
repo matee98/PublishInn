2de7fcef837239ce54cdd774145a8dde68c57030
224464 Zabroń Mateusz <matzab37@gmail.com>
2021-11-20 13:28:18 +0100

    Frontend - added Navbar and React Router.

---
+++ frontend/src/components/HomePage/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home(){
+    return (
+        <div className="Home">
+            <div className="lander">
+                <h1>Cześć wszystkim</h1>
+                <p className="text-muted">Rozgośćcie się</p>
+            </div>
+        </div>
+    );
+}
---
+++ frontend/src/components/partial/Navbar.jsx
@@ -0,0 +1,51 @@
+import {Component} from "react";
+import {Button, Container, Form, FormControl, Nav, Navbar, NavDropdown} from "react-bootstrap";
+
+class NavigationBar extends Component {
+    render() {
+        return(
+            <Navbar bg="light" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand href="/">Navbar scroll</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link href="#action1">Home</Nav.Link>
+                            <Nav.Link href="#action2">Link</Nav.Link>
+                            <NavDropdown title="Link" id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action5">
+                                    Something else here
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#" disabled>
+                                Link
+                            </Nav.Link>
+                        </Nav>
+                        <Form className="d-flex me-auto">
+                            <FormControl
+                                type="search"
+                                placeholder="Szukaj..."
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-success">Szukaj</Button>
+                        </Form>
+                        <div>
+                            <Button variant="outline-success">Zaloguj</Button>
+                            <Button variant="outline-success">Zarejestruj</Button>
+                        </div>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )
+    }
+}
+
+export default (NavigationBar);
