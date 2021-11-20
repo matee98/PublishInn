884ee3d9bf2f870f92c31080cee6cd0b5cb0c469
224464 Zabroń Mateusz <matzab37@gmail.com>
2021-11-20 14:10:22 +0100

    Frontend - not found handling.

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,6 +1,7 @@
 import {Route, Switch} from "react-router-dom";
 import Home from "./components/HomePage/Home";
 import Login from "./components/Login/Login";
+import NotFound from "./components/commonPages/NotFound";
 
 export default function Routes() {
     return (
@@ -10,6 +11,9 @@ export default function Routes() {
             </Route>
             <Route exact path="/login">
                 <Login />
+            </Route>
+            <Route>
+                <NotFound />
             </Route>
         </Switch>
     )
