39f4eaedae51d7822a3a1a1439164f4042d850e2
224464 Zabroń Mateusz <matzab37@gmail.com>
2022-01-23 19:24:36 +0100

    Controller redirecting from error page.

---
+++ src/main/java/com/github/PublishInn/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.github.PublishInn.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@CrossOrigin
+public class IndexController implements ErrorController {
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public ModelAndView saveLeadQuery() {
+        return new ModelAndView("forward:/");
+    }
+
+    public String getErrorPath() {
+        return PATH;
+    }
+}
